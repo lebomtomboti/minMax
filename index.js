5aeb7443b1feb3b5d098d91b7232a9d205c465ce
lebomtomboti <lebomtomboti17@gmail.com>
2024-06-04 16:38:32 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function convertMinutesToSeconds(){
+    let input = document.getElementById('minutesInput').Value;
+    let minutesArray = input.split(',');
+    let resultsElement = document.getElementById('result');
+    resultsElement.innerHTML = '';
+}
+let arraynums = [1,2,3,4,5]
+let min = Math.min(...arraynums)
+let max = Math.max(...arraynums)
+console.log(max)
